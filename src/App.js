151f1dea9fcf821a78757e67ee1e0267f69c44da
sngoyo <salim@kali>
2023-03-25 22:55:17 +0300

    Api code  added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import QuoteMachine from "./Components/QuoteMachine";
 import "./App.css";
+import {useEffect, useState} from "react";
 
 const App = ()=> {
+  const [quoteData, setQuoteData] = useState("");
 
+  useEffect(() =>{
+     fetchQuote();
+  },[]);
+
+  async function fetchQuote() {
+      try{
+          const response = await fetch("https://dummyjson.com/quotes");
+          if(!response.ok){
+              throw new Error(`HTTP error: ${response.status}`);
+          }
+          const data = await response.json();
+          setQuoteData(data);
+
+
+      } catch(error){
+          alert(`Could not get Quote: ${error}, Check your Internet Connection`);
+  }
+}
 
   return(
     <div id="quote box">
-      <QuoteMachine />
+      <QuoteMachine quoteData={quoteData}/>
     </div>
 
 
