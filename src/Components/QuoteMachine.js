048cdafd545d11c1d8eb67d102abf3064fc9d23b
sngoyo <salim@kali>
2023-03-25 22:54:47 +0300

    Api code  added

--- src/Components/QuoteMachine.js
+++ src/Components/QuoteMachine.js
@@ -1,57 +1,70 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import  "/home/salim/Documents/REACT_APPs/quote-app/src/App.css";
-import $ from "jquery";
-
 
 const quoteContainer = [
-  { color: "#395144", quote:"Start writing, no matter what.The water does not flow until the faucet is turned on", author:"Louis L’Amour"},
-  { color: "#FF8787", quote:"Get it down. Take chances. It may be bad, but it's the only way you can do anything really good", author:"William Faulkner"},
-  { color: "#AA8B56", quote:"The first draft is just you telling yourself the story", author:"Terry Pratchett"},
-  { color: "#DC5F00", quote:"Start before you’re ready", author:"Steven Pressfield"},
-  { color: "#BCE29E", quote:"You can always edit a bad page. You can’t edit a blank page", author:"Jodi Picoult"},
-  { color: "#FF5858", quote:"You can’t wait for inspiration. You have to go after it with a club.", author:"Jack London"},
-  { color: "#4649FF", quote:"I have never started a poem yet whose end I knew. Writing a poem is discovering", author:"Robert Frost"},
-  { color: "#3C4048", quote:"If there's a book that you want to read, but it hasn't been written yet, then you must write it.", author:"Toni Morrison"},
-  { color: "#00ABB3", quote:"I'm writing a first draft and reminding myself that I'm simply shoveling sand into a box so that later I can build castles.", author:"Shannon Hale"},
-  { color: "#FF731D", quote:"If you’re always trying to be normal you will never know how amazing you can be", author:"Maya Angelou"}
+  { color: "#222222", quote:"Start writing, no matter what.The water does not flow until the faucet is turned on", author:"Louis L’Amour"},
+  { color: "#FF8787"},{ color: "#AA8B56"},{ color: "#DC5F00"},{ color: "#404258"}, { color: "#3A1078"},
+  { color: "#658864"},{ color: "#2B3467"}, { color: "#00ABB3"}, { color: "#FF731D"},
+  {color: "#0f4a42"}, {color: "#002B5B"}, {color: "#262A56"}, {color: "#251749"}, {color: "#820000"}, {color: "#7B2869"},
+  {color: "#806019"}, {color: "#13005A"}, {color: "#9A208C"}, {color: "#393E46"}, {color: "#7149C6"}, {color: "#FE6244"},
+  {color: "#9e6b47"}, {color: "#00337C"}, {color: "#443C68"}, {color: "##695a5a"}, {color: "#0A2647"}, {color: "#A84448"},
+  {color: "#7d7274"}, {color: "#5909de"}, {color: "#181D31"}
 ]
 
-const QuoteMachine = () => {
-    const [quote, setQuote] = useState(quoteContainer[7].quote);
-    const [color, setColor] = useState(quoteContainer[7].color);
-    const [author, setAthour] = useState(quoteContainer[7].author);
+const QuoteMachine = ({quoteData}) => {
+    const [quote, setQuote] = useState(quoteContainer[0].quote);
+    const [color, setColor] = useState(quoteContainer[0].color);
+    const [author, setAthour] = useState(quoteContainer[0].author);
     document.body.style.background = color;
 
 
     function ChangeQuote () {
+      //Getting random value
+      let randomIndex = Math.floor(Math.random() * (quoteContainer.length-1));
+      //Accessing data from the  Api
+     setQuote(quoteData.quotes[randomIndex].quote);
+     setAthour(quoteData.quotes[randomIndex].author);
 
-      const randomIndex = Math.floor(Math.random() * quoteContainer.length);
-
-     setQuote(quoteContainer[randomIndex].quote);
-      setColor(quoteContainer[randomIndex].color);
-      setAthour(quoteContainer[randomIndex].author);
+     //Acessing data from quoteContainer list
+     setColor(quoteContainer[randomIndex].color);
      document.body.style.background = color;
-
-
 
   }
 
 
     return (
          <div id="quote-box">
-          <h2 id="text" style={{color:color}}> <span className="quote-icon"><i class="fa-solid fa-quote-left"></i></span>
-
-            {quote} <p id="author"><em>-{author}</em></p>  </h2>
-
+          <h2 id="text" style={{color:color}}>
+            <p className="quote-content">
+            <span className="quote-icon">
+              <i class="fa-solid fa-quote-left">
+              </i>
+            </span>
+            {quote}
+            </p>
+            <p id="author">
+              <em>-{author}</em>
+            </p>
+          </h2>
           <div className="icons-button-div">
-
-            <button id="new-quote" onClick={ChangeQuote} style={{backgroundColor:color}}><span className="button-text">New quote</span></button>
-
+            <button id="new-quote" onClick={ChangeQuote} style={{backgroundColor:color}}>
+              <span className="button-text">New quote</span>
+            </button>
             <div id="socialMedia">
-              <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank"><div className="twitter-icon" style={{backgroundColor:color}}><i class="fa-brands fa-twitter"></i></div></a><a href="https://tumblr.com" target="_blank"><div className="tumblr-icon" style={{backgroundColor:color}}><i class="fa-brands fa-tumblr"></i></div></a></div>
+              <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank">
+                <div className="twitter-icon" style={{backgroundColor:color}}>
+                  <i class="fa-brands fa-twitter"></i>
+                  </div>
+                  </a>
+                  <a href="https://tumblr.com" target="_blank">
+                    <div className="tumblr-icon" style={{backgroundColor:color}}>
+                      <i class="fa-brands fa-tumblr"></i>
+                    </div>
+                    </a>
+                    </div>
           </div>
-          <p className="mtunzi">by Salim</p>
+          <p className="mtunzi">By Salim</p>
          </div>
 
 
